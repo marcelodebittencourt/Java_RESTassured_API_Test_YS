ed4cd265b3e00b94caac124eea9c7191230d8009
marcelodebittencourt <tchelopoa@gmail.com>
2021-09-27 13:24:25 -0300

    Code refactoring to improve manutenability

--- src/test/java/tests/GetLondonUKWeather.java
+++ src/test/java/tests/GetLondonUKWeather.java
@@ -1,15 +1,31 @@
 package tests;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetLondonUKWeather {
+    @BeforeAll
+    public static void setup() {
+        baseURI = "http://api.openweathermap.org/data/2.5";
+    }
+
     @Test
     public void getLondonUKWeather_expectedLondonCity() {
-        when().
-                get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7488f5cd3fbbc91c90332c7d4e0af61a").
-        then().
-                statusCode(200);
+        String city = "London";
+        String countryCode = "uk";
+        String appid = "7488f5cd3fbbc91c90332c7d4e0af61a";
+
+        given()
+                .queryParam("q", city + "," + countryCode)
+                .queryParam("appid", appid)
+        .when()
+                .get("/weather")
+        .then()
+                .statusCode(200)
+                .assertThat().body("name", equalTo(city))
+        ;
     }
 }
